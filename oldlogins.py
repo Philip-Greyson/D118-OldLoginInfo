789bdb86eab0505439188fa12803ca4c583d3fd9
FillGee <pgreyson@d118.org>
2022-09-26 15:19:15 -0500

    implemented log file for future troubleshooting

--- oldlogins.py
+++ oldlogins.py
@@ -34,40 +34,45 @@ schoolNumbers = ['5', '1003', '1004', '2001', '2005', '2007', '2', '133', '300',
 with oracledb.connect(user=un, password=pw, dsn=cs) as con:
     with con.cursor() as cur:  # start an entry cursor
         with pysftp.Connection(sftpHOST, username=sftpUN, password=sftpPW, cnopts=cnopts) as sftp:
-            print("Connection established: " + con.version)
-            print('SFTP connection established')
-            # print(sftp.pwd)  # debug, show what folder we connected to
-            # print(sftp.listdir())  # debug, show what other files/folders are in the current directory
-            for index, school in enumerate(schoolList): # go through each school in the list one at a time, allows us to search for only that building and output to individual files
-                #print(str(index) + ': ' + school + '|' + schoolNumbers[index])
-                filename = school + '-Old-Logins.txt' # make a unique filename for each output file
-                schoolcode = schoolNumbers[index] # the school numeric code by passing the index of our current school to the related numbers list
-                sftp.chdir('/sftp/oldLogins/' + school)  # change to the correct building subfolder
-                print(sftp.pwd)  # debug, make sure out changedir worked
-                # print(sftp.listdir())
-                with open (filename, 'w') as output:
-                    print("Starting " + school)
-                    # print(filename) # debug
-                    # print(schoolcode) # debug
-                    # do a query of all staff who have the building as their homeschool (to avoid duplicates across district) and who are active
-                    cur.execute("SELECT lastfirst, teachernumber, email_addr FROM teachers WHERE homeschoolid = " + schoolcode + " AND schoolid = " + schoolcode + " AND status = 1 AND email_addr IS NOT NULL ORDER BY users_dcid DESC")
-                    teachers = cur.fetchall()
-                    for count, teacher in enumerate(teachers):
-                        try: # do each teacher in try/except blocks so we can skip a user without breaking the whole thing
-                            sys.stdout.write('\rProccessing staff entry %i' % count) # sort of fancy text to display progress of how many students are being processed without making newlines
-                            sys.stdout.flush()
-                            name = str(teacher[0])
-                            teacherNum = str(teacher[1])
-                            email = str(teacher[2])
-                            login = email.split('@')[0] # split the string with email by the @ symbol and only take the first part
-                            login = login[0:20] # take only the first 20 characters if their name is longer, as that is the max length for the login field in PS
-                            # print('\n' + login) # debug
-                            # print the fields out to the output file, duplicate the logins and passwords since there is both teacher and admin side login info
-                            print(name + '\t' + teacherNum + '\t' + login + '\t' + login + '\t' + password + '\t' + password, file=output)
-                        except Exception as er:
-                            print("Error on user " + str(teacher[0]) + str(er))
-                
-                sftp.put(filename) # upload the output file to the sftp server in the correct school directory
-                print('\n' + filename + " placed on the remote server")
-                print('------------------------') # visual divider between school entries
+            with open('login_log.txt', 'w') as outputLog:
+                print("Connection established: " + con.version)
+                print('SFTP connection established')
+                # print(sftp.pwd)  # debug, show what folder we connected to
+                # print(sftp.listdir())  # debug, show what other files/folders are in the current directory
+                for index, school in enumerate(schoolList): # go through each school in the list one at a time, allows us to search for only that building and output to individual files
+                    #print(str(index) + ': ' + school + '|' + schoolNumbers[index])
+                    filename = school + '-Old-Logins.txt' # make a unique filename for each output file
+                    schoolcode = schoolNumbers[index] # the school numeric code by passing the index of our current school to the related numbers list
+                    sftp.chdir('/sftp/oldLogins/' + school)  # change to the correct building subfolder
+                    print(sftp.pwd)  # debug, make sure out changedir worked
+                    print(sftp.pwd, file=outputLog)  # debug, make sure out changedir worked
+                    # print(sftp.listdir())
+                    with open (filename, 'w') as output:
+                        print("Starting " + school)
+                        print("Starting " + school, file=outputLog)
+                        print(filename, file=outputLog) # debug
+                        print(schoolcode, file=outputLog) # debug
+                        # do a query of all staff who have the building as their homeschool (to avoid duplicates across district) and who are active
+                        cur.execute("SELECT lastfirst, teachernumber, email_addr FROM teachers WHERE homeschoolid = " + schoolcode + " AND schoolid = " + schoolcode + " AND status = 1 AND email_addr IS NOT NULL ORDER BY users_dcid DESC")
+                        teachers = cur.fetchall()
+                        for count, teacher in enumerate(teachers):
+                            try: # do each teacher in try/except blocks so we can skip a user without breaking the whole thing
+                                sys.stdout.write('\rProccessing staff entry %i' % count) # sort of fancy text to display progress of how many students are being processed without making newlines
+                                sys.stdout.flush()
+                                name = str(teacher[0])
+                                teacherNum = str(teacher[1])
+                                email = str(teacher[2])
+                                login = email.split('@')[0] # split the string with email by the @ symbol and only take the first part
+                                login = login[0:20] # take only the first 20 characters if their name is longer, as that is the max length for the login field in PS
+                                print(name + ' | ' + login, file=outputLog) # debug
+                                # print the fields out to the output file, duplicate the logins and passwords since there is both teacher and admin side login info
+                                print(name + '\t' + teacherNum + '\t' + login + '\t' + login + '\t' + password + '\t' + password, file=output)
+                            except Exception as er:
+                                print("Error on user " + str(teacher[0]) + str(er))
+                                print("Error on user " + str(teacher[0]) + str(er), file=outputLog)
+                    
+                    sftp.put(filename) # upload the output file to the sftp server in the correct school directory
+                    print('\n' + filename + " placed on the remote server")
+                    print(filename + " placed on the remote server", file=outputLog)
+                    print('------------------------') # visual divider between school entries
 
